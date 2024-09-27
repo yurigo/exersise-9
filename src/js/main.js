16f903916e5aa375a760acd5b1eaeb2f09baabf9
Francesc Constans <fconstans@lasalle.es>
2024-09-27 16:32:18 +0200

    change UI

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,1 @@
-const a = 0;
-
-document.getElementById('helloWorld').innerHTML = `Hello World!' ${a}`;
-
-// console.log('hello');
+document.getElementById('helloWorld').innerHTML = 'Hello World!';
